d141fab6ed6819f210b7cae17d9638418edeafcc
Paria Sarzaeim <59663526+Pariasrz@users.noreply.github.com>
2023-02-28 22:29:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, make_response
 from PIL import Image, ImageFilter
 import io
 app = Flask(__name__)
@@ -18,12 +18,10 @@ def favicon():
 @app.route('/hello', methods=['POST'])
 def hello():
    name = request.form.get('name')
-   # Get the image file from the form
    img_file = request.files['image']
    # Open the image file and apply blur filter
    img = Image.open(img_file)
    blurred_img = img.filter(ImageFilter.BLUR)
-
    # Convert the blurred image to bytes and store in memory
    img_bytes = io.BytesIO()
    blurred_img.save(img_bytes, format='PNG')
@@ -31,7 +29,12 @@ def hello():
    
    if name:
        print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name, blurred_img=img_bytes.getvalue())
+       # Create a response with the blurred image
+       response = make_response(render_template('hello.html', name=name))
+       response.headers.set('Content-Type', 'image/png')
+       response.headers.set('Content-Disposition', 'inline', filename='blurred_image.png')
+       response.set_data(img_bytes.getvalue())
+       return response
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
