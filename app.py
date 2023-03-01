c8e56a8aca7eff920129e70bed109a03600e5544
Paria Sarzaeim <59663526+Pariasrz@users.noreply.github.com>
2023-02-28 23:00:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, make_response
 from PIL import Image, ImageFilter
 import io
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+
 app = Flask(__name__)
+
+# Blob Storage Configuration
+CONNECTION_STRING = os.environ['DefaultEndpointsProtocol=https;AccountName=coviddiag;AccountKey=HcpkgvaN8ppaUO6f8EYWdhHmT9ZZ902NBq3p/We1tAMBVMeI36GfMUpFInLMIF0DqiUnWuAqW6pu+ASt9oHmdA==;EndpointSuffix=core.windows.net']
+CONTAINER_NAME = os.environ['imageprocess']
+blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
+container_client = blob_service_client.get_container_client(CONTAINER_NAME)
 
 
 @app.route('/')
@@ -27,17 +35,27 @@ def hello():
    blurred_img.save(img_bytes, format='PNG')
    img_bytes.seek(0)
    
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       # Create a response with the blurred image
-       response = make_response(render_template('hello.html', name=name))
-       response.headers.set('Content-Type', 'image/png')
-       response.headers.set('Content-Disposition', 'inline', filename='blurred_image.png')
-       response.set_data(img_bytes.getvalue())
-       return response
+   # Get the username and password from the form
+   username = request.form.get('username')
+   password = request.form.get('password')
+   
+   if username and password:
+      print('Request for hello page received with name=%s and username=%s, password=%s' % (name, username, password))
+       
+      # Store the username and password in Azure Blob Storage
+      blob_name = f"{username}.txt"
+      blob_client = container_client.get_blob_client(blob_name)
+      blob_client.upload_blob(password)
+      
+      # Create a response with the blurred image
+      response = make_response(render_template('hello.html', name=name))
+      response.headers.set('Content-Type', 'image/png')
+      response.headers.set('Content-Disposition', 'inline', filename='blurred_image.png')
+      response.set_data(img_bytes.getvalue())
+      return response
    else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+      print('Request for hello page received with no name or blank name -- redirecting')
+      return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
